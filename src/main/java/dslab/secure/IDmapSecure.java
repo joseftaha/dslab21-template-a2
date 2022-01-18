974e7afd8779b0cbbf47b5006c2bd3800e8c5813
Oliver Theimer <e51840817@student.tuwien.ac.at>
2022-01-18 16:12:19 +0100

    added documentation to IDmapSecure

--- src/main/java/dslab/secure/IDmapSecure.java
+++ src/main/java/dslab/secure/IDmapSecure.java
@@ -2,12 +2,26 @@ package dslab.secure;
 
 public interface IDmapSecure {
 
+    /**
+     * client performs the handshake with the server
+     */
     void performHandshakeClient();
 
+    /**
+     * server performs handshake with the client
+     */
     void performHandshakeServer();
 
+    /**
+     * encrypts and sends a message
+     * @param message
+     */
     void sendMessage(String message);
 
+    /**
+     * receives and decrypts a message
+     * @return a human-readable representation of the message
+     */
     String readMessage();
 
 }
